e856cf2c72970b99bd943588b9ad2c31449bd46f
Imtiyaaz-D <imtdamon777@gmail.com>
2023-08-22 14:00:33 +0200

    test1.3

--- api/model/User.js
+++ api/model/User.js
@@ -1,5 +1,5 @@
 const db = require('../config')
-// const {hash ,compare ,hashSync} = require('bcrypt')
+const {hash ,compare ,hashSync} = require('bcrypt')
 class Users{
     fetchUsers(req, res){
         const query = `
@@ -28,6 +28,101 @@ class Users{
             })
         })
     }
-
+   async registerUser(req,res){
+    const data  = req.body()
+        data.userPass = await hash(data.userPass, 15)
+        // payload
+        const user = {
+            emailAdd: data.emailAdd,
+            userPass: data.userPass
+        }
+        // Query
+        const query = `
+        INSERT INTO Users
+        SET ?
+        `
+        db.query(query, [data],(err)=>{
+            if(err) throw err
+            res.cookies("LegitUser", token,
+            {
+                maxAge: 3600000,
+                httpOnly: true
+            })
+            res.json({
+                status: res.statusCode,
+                msg:"you are now registered."
+            })
+        })
+    }
+    login(req,res){
+        const {emailAdd, userPass} =
+        req.body
+        const query =
+        `
+        SELECT firstName , lastName , gender , userRole , emailAdd , userPass , userProfile
+        FROM Users
+        WHERE emailAdd = '${emailAdd}';
+        `
+        db.query(query,async(err, results)=>{
+            if(err) throw err
+            if(!results?.length){
+                res.json({
+                    status:res.statusCode,
+                    msg : "You provided a wrong email."
+                })
+            }else{
+                await compare(userPass, results[0].userPass,(cErr,cResults)=>{
+                    if(cErr) throw cErr
+                    // Create a token
+                    const token = createToken({
+                        emailAdd, userPass
+                    })
+                    // Save a token
+                    res.cookies("LegitUser",
+                    token,{
+                        maxAge: 3600000,
+                        httpOnly: true
+                    }
+                    )
+                    if(cResults){
+                        res.json({
+                            msg: "logged in",
+                            token,
+                            results: results[0]
+                        })
+                    }else{
+                        res.json({
+                            status: statusCode,
+                            msg: "Invalid password or you have not registered"
+                        })
+                    }
+                })
+            }
+        })
+    }
+    updateUser(req, res){
+        const data = req.body
+        if(data.userPass){
+            data.userPass = 
+            hashSync(data.userPass, 15)
+        }
+        const query = `
+        UPDATE Users SET ?
+        WHERE userID = ?
+        `
+    }
+    deleteUser(req, res){
+        const query = 
+        `
+        DELETE FROM Users WHERE UserID = ${req.params.id};
+        `
+        db.query(query,(err)=>{
+            if(err) throw err
+            res.json({
+                status:statusCode,
+                msg: "A user record was deleted"
+            })
+        })
+    }
 }
 module.exports = Users
