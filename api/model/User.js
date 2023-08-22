ea0bcd8e8a29d64eabed8984bb2e8e97737d9d9b
Imtiyaaz-D <imtdamon777@gmail.com>
2023-08-22 12:21:32 +0200

    test 1.2

--- api/model/User.js
+++ api/model/User.js
@@ -1,6 +1,5 @@
 const db = require('../config')
-const {hash ,compare ,hashSync} = require('bcrypt')
-
+// const {hash ,compare ,hashSync} = require('bcrypt')
 class Users{
     fetchUsers(req, res){
         const query = `
@@ -18,7 +17,7 @@ class Users{
     fetchUser(req, res){
         const query = `
         SELECT userID ,firstName, lastName, gender, userRole, emailAdd, userPass, userProfile
-        FROM USER
+        FROM Users
         WHERE userID = ${req.params.id};
         `
         db.query(query,(err,results)=>{
